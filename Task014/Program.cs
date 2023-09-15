f662d8efcfc3cee114e64f4ab32bc5807519817e
elena bobina <putnyk19@mail.ru>
2023-09-15 14:50:48 +0500

    Task014

---
+++ Task014/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+int[] GetArray(int length, int min, int max)
+{
+    int[] arr = new int[length];
+    Random rand = new Random();
+    for (int i = 0; i < length; i++)
+        arr[i] = rand.Next(min, max + 1);
+    return arr;
+}
+
+int[] array = GetArray(10, 100, 1000);
+System.Console.WriteLine(string.Join(' ', array));
+int Even(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 0)
+        count++;
+    }
+    return count;
+}
+
+int result = Even(array);
+System.Console.WriteLine($"В массиве {result} четных чесел");
